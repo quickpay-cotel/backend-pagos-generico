33931bebe79ce8cebc686d7d5ac2937616e6b252
alvaroquispesegales <alvaroquispesegales@outlook.com>
2025-06-25 19:27:08 -0400

    seha parametrizado el ssl en  mainjs

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { AllExceptionsFilter } from './common/filters/http-exception.filter';
-import { SuccessResponseInterceptor } from './common/interceptors/success-response.interceptor';
-import { ValidationPipe } from '@nestjs/common';
-
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { AllExceptionsFilter } from "./common/filters/http-exception.filter";
+import { SuccessResponseInterceptor } from "./common/interceptors/success-response.interceptor";
+import { ValidationPipe } from "@nestjs/common";
+import * as fs from "fs";
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let app;
+
+  if (process.env.NODE_ENV === "production") {
+    // Configuración con SSL en servidor
+    const httpsOptions = {
+      key: fs.readFileSync("/etc/ssl/quickpay.com.bo/private.key"),
+      cert: fs.readFileSync("/etc/ssl/quickpay.com.bo/certificate.crt"),
+      ca: fs.readFileSync("/etc/ssl/quickpay.com.bo/ca_bundle.crt"), // Si tienes un certificado intermedio
+    };
+    app = await NestFactory.create(AppModule, { httpsOptions });
+  } else {
+    // Configuración sin SSL en local
+    app = await NestFactory.create(AppModule);
+  }
+
   app.enableCors(); // <-- aquí
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix("api/v1");
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new SuccessResponseInterceptor());
   // Habilitar validación global para los DTOs
